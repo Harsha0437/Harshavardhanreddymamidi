403f939addc485d60cd60d959f0f6ff66eb85d24
HARSHA MAMIDI <harshamamidi@harshas-Air.lan>
2024-01-12 11:19:05 -0600

    Day02

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String name ="harshavardhan";
+    public String getName(){
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.getName());
+    }
+}
+
